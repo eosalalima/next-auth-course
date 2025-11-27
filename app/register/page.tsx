55dbc96b481c8a4ce68ffceb5ce28b723bbbceeb
Eric O. Salalima <eosalalima@gmail.com>
2025-11-27 05:08:49 +0000

    Setup Register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,12 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Register() {
-    return <div>Register Page</div>;
+    return <main className="flex justify-center items-center min-h-screen ">
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>Register</CardTitle>
+                <CardDescription>Register for a new account.</CardDescription>
+            </CardHeader>
+        </Card>
+    </main>;
 }
