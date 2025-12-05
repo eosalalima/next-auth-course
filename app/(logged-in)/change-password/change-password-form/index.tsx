f6738d29ec583d91b659f136ea1f70c57eb9729a
Eric O. Salalima <eosalalima@gmail.com>
2025-12-05 01:15:19 +0000

    Minor changes

---
+++ app/(logged-in)/change-password/change-password-form/index.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { passwordMatchSchema } from "@/validation/passwordMatchSchema";
+import { passwordSchema } from "@/validation/passwordSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, useForm } from "react-hook-form";
+import z from "zod";
+
+const formSchema = z.object({
+    currentPassword: passwordSchema
+}).and(passwordMatchSchema);
+
+export default function ChangePasswordForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            currentPassword: "",
+            password: "",
+            confirmPassword: ""
+        }
+    });
+    
+    const handleSubmit = async (data: z.infer<typeof formSchema>) => {}
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)}>
+                <fieldset disabled={form.formState.isSubmitting} className="flex flex-col gap-4">
+                    <FormField 
+                        control={form.control}                                    
+                        name="currentPassword" 
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Current Password</FormLabel>
+                                <FormControl>
+                                    <Input {...field} type="password" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}                            
+                        />
+
+                    <FormField                             
+                        control={form.control}
+                        name="password" 
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input {...field} type="password" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}                            
+                        />
+
+                    <FormField 
+                        control={form.control}
+                        name="confirmPassword" 
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Confirm Password</FormLabel>
+                                <FormControl>
+                                    <Input {...field} type="password" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}                            
+                        />
+
+                    <Button type="submit">
+                        Register
+                    </Button>     
+                </fieldset>                   
+            </form>
+        </Form>
+    );
+}
--- app/(logged-in)/change-password/page.tsx
+++ app/(logged-in)/change-password/page.tsx
@@ -1,11 +1,15 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import ChangePasswordForm from "./change-password-form";
 
-export default function ChangePassword() {
+export default function ChangePassword() {    
     return (
-        <Card>
+        <Card className="w-[350px]">
             <CardHeader>
                 <CardTitle>Change Password</CardTitle>
             </CardHeader>
+            <CardContent>
+                <ChangePasswordForm />
+            </CardContent>
         </Card>
     );
 }
